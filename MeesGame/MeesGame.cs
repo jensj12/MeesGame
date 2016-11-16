15bdb4265001dd77efa626dfc56aecf4e3173db8
jensj12 <jensheuseveldt@gmail.com>
2016-11-16 14:10:10 +0100

    GameManagement

---
+++ MeesGame/MeesGame.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace MeesGame
+{
+    public class MeesGame : GameEnvironment
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MeesGame())
+                game.Run();
+        }
+
+        public MeesGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            screen = new Point(1440, 825);
+            windowSize = new Point(1024, 586);
+            FullScreen = false;
+            /* uncomment to add gamestates
+            gameStateManager.AddGameState("titleMenu", new TitleMenuState());
+            gameStateManager.AddGameState("helpState", new HelpState());
+            gameStateManager.AddGameState("playingState", new PlayingState(Content));
+            gameStateManager.AddGameState("levelMenu", new LevelMenuState());
+            gameStateManager.AddGameState("gameOverState", new GameOverState());
+            gameStateManager.AddGameState("levelFinishedState", new LevelFinishedState());
+            gameStateManager.SwitchTo("titleMenu");//*/
+        }
+    }
+}
