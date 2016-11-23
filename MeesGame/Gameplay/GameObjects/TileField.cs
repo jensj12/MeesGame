c824e50d28076f362a3a5dbaee6809a9605b4390
jensj12 <jensheuseveldt@gmail.com>
2016-11-23 13:22:46 +0100

    Make tiles and levels work

--- MeesGame/Gameplay/GameObjects/TileField.cs
+++ MeesGame/Gameplay/GameObjects/TileField.cs
@@ -2,9 +2,19 @@
 {
     class TileField : GameObjectGrid
     {
-        public TileField (int rows, int columns, int layer = 0, string id = "") : base(rows, columns, layer, id)
+        public TileField(int rows, int columns, int layer = 0, string id = "") : base(rows, columns, layer, id)
         {
 
+        }
+
+        public TileType GetType(int x, int y)
+        {
+            if (x < 0 || y < 0 || x > Columns || y > Rows)
+            {
+                return TileType.wall;
+            }
+            Tile tile = Get(x, y) as Tile;
+            return tile.TileType;
         }
     }
 }
--- MeesGame/Gameplay/GameStates/PlayingLevelState.cs
+++ MeesGame/Gameplay/GameStates/PlayingLevelState.cs
@@ -1,36 +1,42 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace MeesGame
 {
     class PlayingLevelState : IGameLoopObject
     {
         ContentManager content;
+        List<Level> level;
+        int currentLevelIndex;
 
         public PlayingLevelState(ContentManager content)
         {
             this.content = content;
+            level = new List<Level>();
+            level.Add(new Level());
+            currentLevelIndex = 0;
         }
 
         public void HandleInput(InputHelper inputHelper)
         {
-
+            level[currentLevelIndex].HandleInput(inputHelper);
         }
 
         public void Update(GameTime gameTime)
         {
-
+            level[currentLevelIndex].Update(gameTime);
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            
+            level[currentLevelIndex].Draw(gameTime, spriteBatch);
         }
 
         public void Reset()
         {
-
+            level[currentLevelIndex].Reset();
         }
     }
 }
