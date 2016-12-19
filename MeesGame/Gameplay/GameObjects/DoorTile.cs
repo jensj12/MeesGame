a620bf6e214ac54b525742bdffa8f19bcca7b500
nessemis <dennis.ar5@gmail.com>
2016-12-19 08:20:45 +0100

    incremental

--- MeesGame/Gameplay/GameObjects/DoorTile.cs
+++ MeesGame/Gameplay/GameObjects/DoorTile.cs
@@ -1,15 +1,13 @@
-﻿using static MeesGame.PlayerAction;
-
-namespace MeesGame
+﻿namespace MeesGame
 {
     class DoorTile : WallTile
     {
         public const string defaultAssetName = "horizontalDoorOverlay";
+        protected bool doorIsOpen = false;
 
         //TODO: Pair a door with a specific key.
         public DoorTile(int layer = 0, string id = "") : base("horizontalDoorOverlay", TileType.Door, layer, id)
         {
-
         }
 
         //TODO implement doorIsOpen
@@ -18,55 +16,48 @@ namespace MeesGame
             //A player is allowed to move onto a door tile if the door is open.
             //if (DoorIsOpen(player))
             if (player.HasKey())
-                return true;
-            else
-                return false;
-        }
-
-        public override bool IsActionForbiddenFromHere(Player player, PlayerAction action)
-        {
-            //A player can only move back to where he came from or into the opposite direction
-            if (action == EAST || action == WEST)
-            {
-                if (player.LastAction == EAST || player.LastAction == WEST)
-                    return false;
-                else
+                if (doorIsOpen)
                     return true;
-            }
-            else if (action == NORTH || action == SOUTH)
-            {
-                if (player.LastAction == NORTH || player.LastAction == SOUTH)
-                    return false;
                 else
-                    return true;
-            }
-            else
-                return true;
+                    return false;
+            return true;
         }
 
         /*TODO 
         public bool DoorIsOpen(Player player)
+        public void OpenDoor(Player player)
         {
             //A player is allowed to open a door if he has the right key.
             bool heeftSleutel = player.PlayerHasKey();
             return false;
         }*/
+            //A player is only allowed to open a door if he has the right key.
+            if (player.HasKey())
+            {
+                doorIsOpen = true;
+            }
+}
 
-        public override void UpdateGraphicsToMatchSurroundings()
+public override void UpdateGraphicsToMatchSurroundings()
+{
+@@ -63,11 + 68,21 @@ namespace MeesGame
+{
+    public const string defaultAssetName = "keyOverlay";
+
+<<<<<<< HEAD
+         public KeyTile(int layer = 0, string id = "") : base(defaultAssetName, TileType.Key, layer, id)
+=======
+        public KeyTile(int layer = 0, string id = "") : base("keyOverlay", TileType.Key, layer, id)
+>>>>>>> refs/remotes/origin/master
         {
-            //TODO: choose hor/ver wall
-        }
-    }
-
-
-    class KeyTile : FloorTile
-    {
-        public const string defaultAssetName = "keyOverlay";
-
-        public KeyTile(int layer = 0, string id = "") : base(defaultAssetName, TileType.Key, layer, id)
-        {
 
         }
+
+public override InventoryItem GetItem()
+{
+    InventoryItem key = new InventoryKey();
+    return key;
+}
 
         //TODO: if key has been picked up, keytile changes into floortile.
     }
