c5445fc7a972959e9c77fc3ece1282d8f5d563b8
jorisBarkema <jorisbarkema@gmail.com>
2017-01-07 19:28:07 +0100

    Fix #113: Standing player image (#118)

--- MeesGame/Gameplay/GameObjects/AnimatedMovingGameObject.cs
+++ MeesGame/Gameplay/GameObjects/AnimatedMovingGameObject.cs
@@ -29,7 +29,7 @@ namespace MeesGame
             }
             else
             {
-                Sprite.SheetIndex = Sprite.SpriteRowIndex;
+                Sprite.SheetIndex = Sprite.SpriteRowIndex * Sprite.NumberColumns;
             }
         }
     }
