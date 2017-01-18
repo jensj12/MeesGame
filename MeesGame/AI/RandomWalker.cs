6bd4acafbdd44d9dd55d8e015d0b5568ada738df
mmeijer <meijermax99@gmail.com>
2017-01-18 18:53:18 +0100

    Make AI run on separate thread

--- MeesGame/AI/RandomWalker.cs
+++ MeesGame/AI/RandomWalker.cs
@@ -1,20 +1,40 @@
 ﻿using MeesGame;
+using System.Threading;
 
 namespace AI
 {
+    /// <summary>
+    /// An AI whose decisions are distributed uniformly across the set of possible actions.
+    /// The AI will wait at least 50 ms between actions
+    /// </summary>
     class RandomWalker : IAI
     {
         IAIPlayer player;
+        PlayerAction nextAction = PlayerAction.NONE;
 
         public void GameStart(IAIPlayer player, int difficulty)
         {
             this.player = player;
         }
 
-        public PlayerAction GetNextAction()
+        public void ThinkAboutNextAction()
         {
+            nextAction = PlayerAction.NONE;
             IPlayer dummy = player.DummyPlayer;
-            return dummy.PossibleActions[GameEnvironment.Random.Next(dummy.PossibleActions.Count)];
+
+            //scientists have claimed sleeping helps you make better decisions
+            //granted, it was only tested on humans, but it probably applies to computers as well
+            Thread.Sleep(50);
+
+            nextAction = dummy.PossibleActions[GameEnvironment.Random.Next(dummy.PossibleActions.Count)];
+        }
+
+        public void UpdateNextAction()
+        {
+            if (player.DummyPlayer.CanPerformAction(nextAction))
+                player.NextAIAction = nextAction;
+            else
+                player.NextAIAction = PlayerAction.NONE;
         }
     }
 }
